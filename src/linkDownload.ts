0ac250034afc2bc674e1e62b645577b6d195cbd1
当时只道是寻常 <1207588603@qq.com>
2024-10-28 16:31:52 +0800

    feat: linkDownload 支持 URL、File 对象和 Blob 对象

--- src/linkDownload.ts
+++ src/linkDownload.ts
@@ -1,16 +1,18 @@
 import { isBrowser } from './isBrowser'
 
-/** 创建一个临时的链接，用于下载指定 URL 的文件 */
-export function linkDownload(fileURL: string, fileName?: string): void {
+/**
+ * 创建一个临时的链接，用于下载指定 URL 的文件 支持 URL、File 对象和 Blob 对象下载
+ */
+export function linkDownload(fileURL: string | Blob | File, fileName?: string): void {
   // 判断当前是否为浏览器环境
   if (!isBrowser()) throw new Error('此方法仅支持在浏览器环境运行')
+  let href: string = typeof fileURL === 'string' ? fileURL : URL.createObjectURL(fileURL)
   const a = document.createElement('a')
-  a.style.position = 'relative'
-  a.style.left = '-999999px'
-  a.style.top = '-999999px'
-  a.href = fileURL
+  a.style.display = 'none'
+  a.href = href
   a.download = fileName || Date.now().toString()
   document.body.appendChild(a)
   a.click()
   document.body.removeChild(a)
+  if (typeof fileURL !== 'string') URL.revokeObjectURL(href)
 }
